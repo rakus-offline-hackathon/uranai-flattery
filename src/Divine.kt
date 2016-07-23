48881cf84fb9e69bc3c3827fef288098b3948155
HomMarkHunt <honmaso@gmail.com>
2016-07-23 16:46:28 +0900

    create awesome helpers.

--- src/Divine.kt
+++ src/Divine.kt
@@ -1,3 +1,27 @@
-/**
- * Created by homma on 2016/07/23.
- */
+fun calc(vararg exerted: Int): Int {
+    val ans: String = exerted.sum().toString()
+    return ans.substring(ans.length - 1).toInt()
+}
+
+fun getDivine(ordinal: Int): String {
+    val types = Divine.values()
+    for (type in types) {
+        if (type.ordinal == ordinal) {
+            return type.toString();
+        }
+    }
+    return errorMessage
+}
+
+enum class Divine {
+    マジで一番いい,
+    一日中笑顔,
+    人によっては幸せな日,
+    痛いことはない,
+    普通,
+    電車が遅延する,
+    かなりの痛みを味わう,
+    退職する,
+    仲いい人全員と一生会えない,
+    死ぬ;
+}
--- src/Validate.kt
+++ src/Validate.kt
@@ -1,3 +1,15 @@
-/**
- * Created by homma on 2016/07/23.
- */
+fun isValidName(name: String): Boolean {
+    if (name.isNullOrBlank()) {
+        return false
+    }
+    return true
+}
+
+fun isValidBirthday(birthday: String): Boolean {
+    if (birthday.isNullOrBlank()) {
+        return false
+    }
+
+    val regex = Regex("^({4})(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])$")
+    return !regex.matches(birthday)
+}
