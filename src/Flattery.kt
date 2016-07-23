eacfe8429756907d167c3b58ad4aa9186b0fed6f
HomMarkHunt <honmaso@gmail.com>
2016-07-23 16:47:03 +0900

    final!! add main logic.
    I never touch this.Lol

--- src/Flattery.kt
+++ src/Flattery.kt
@@ -1,3 +1,28 @@
+import java.time.ZonedDateTime
+import java.util.Scanner
+
 fun main(args: Array<String>) {
-    println("Hello World!!")
+
+    println(welcome)
+    val scanner = Scanner(System.`in`)
+
+    println(nameScan)
+    val name = scanner.next()
+    if (!isValidName(name)) {
+        println(errorMessage)
+        System.exit(1)
+    }
+
+    println(birthdayScan)
+    val birthday = scanner.next()
+    if (!isValidBirthday(birthday)) {
+        println(errorMessage)
+        System.exit(1)
+    }
+
+    val salt = ZonedDateTime.now().dayOfMonth;
+    val destiny = calc(name.length, birthday.toInt(), salt)
+    val result = getDivine(destiny);
+    println(seeya)
+    println(result)
 }
